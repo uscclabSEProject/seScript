bb78990f1386bcf3003d907be31fd5fe12d8c657
amikai <as23041248@gmail.com>
2017-11-02 11:18:40 +0800

    Fix the error by toggling sebool

--- script_for_local.sh
+++ script_for_local.sh
@@ -4,11 +4,11 @@ if [ `id -u` != "0" ] ; then
 	exit 1
 fi
 
-setsebool -P secure_mode_insmod 1
-setsebool -P secoure_mode_policyload 1
-setsebool -P user_tcp_server 1 
-setsebool -P spamassassin_can_network 1
-setsebool -P cdrecord_read_content 1
-setsebool -P allow_ssh_keysign 1
-setsebool -P keygen_disable_trans 1
+setsebool -P secure_mode_insmod 0
+setsebool -P secure_mode_policyload 0
+setsebool -P user_tcp_server 0
+setsebool -P spamassassin_can_network 0
+setsebool -P cdrecord_read_content 0
+setsebool -P allow_ssh_keysign 0
+setsebool -P keygen_disable_trans 0
 
